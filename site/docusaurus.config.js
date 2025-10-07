eafebbb840e99710a812b5518db0e82af66a53c5
Rick Ossendrijver <rick.ossendrijver@gmail.com>
2025-10-07 10:31:49 +0200

    Add some more things on first page

--- site/docusaurus.config.js
+++ site/docusaurus.config.js
@@ -11,7 +11,7 @@ import {themes as prismThemes} from 'prism-react-renderer';
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: 'Break your testing habits',
-  tagline: 'Adopt better testing practices with automated refactoring',
+  tagline: 'Adopt better testing practices with automated refactoring.',
   favicon: 'img/favicon.ico',
 
   // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
